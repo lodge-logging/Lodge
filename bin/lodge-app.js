f987c2a46d477c98a9b956a8ef526a16408f5716
Sam Clark <clarksam19@gmail.com>
2021-11-17 15:42:10 -0600

    dynamically add ips to es instance config using context
    Co-authored-by: RanaDeeb <62123706+ranadeeb92@users.noreply.github.com>
    Co-authored-by: ReginaDonovan <55225918+rgdonovan@users.noreply.github.com>
    Co-authored-by: JustinLo <50888159+justinnnlo@users.noreply.github.com>

--- bin/lodge-app.js
+++ bin/lodge-app.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const cdk = require("@aws-cdk/core");
 const { LodgeAppStack } = require("../lib/lodge-app-stack");
+const output = require('../output.json');
 
-const app = new cdk.App();
+const app = new cdk.App({context: {output}});
 new LodgeAppStack(app, "LodgeAppStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -18,4 +19,5 @@ new LodgeAppStack(app, "LodgeAppStack", {
   // env: { account: '123456789012', region: 'us-east-1' },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
 
