42122c3f165eacaabd33bdf45eb9b731b1007348
ranadeeb92 <62123706+ranadeeb92@users.noreply.github.com>
2021-11-19 15:12:06 -0800

    updated
    Co-authored-by: SamClark <63471713+clarksam19@users.noreply.github.com>
    Co-authored-by: ReginaDonovan <55225918+rgdonovan@users.noreply.github.com>
    Co-authored-by: JustinLo <50888159+justinnnlo@users.noreply.github.com>

--- bin/lodge-app.js
+++ bin/lodge-app.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 const cdk = require("@aws-cdk/core");
 const { LodgeAppStack } = require("../lib/lodge-app-stack");
+const { KafkaStage } = require("../lib/lodge-app-rana-stack");
 const output = require("../output.json");
 const app = new cdk.App({ context: { output } });
-new LodgeAppStack(app, "LodgeAppStack", {
-  //   /* If you don't specify 'env', this stack will be environment-agnostic.
-  //    * Account/Region-dependent features and context lookups will not work,
-  //    * but a single synthesized template can be deployed anywhere. */
-  //   /* Uncomment the next line to specialize this stack for the AWS Account
-  //    * and Region that are implied by the current CLI configuration. */
-  //   // env: {
-  //   //   account: process.env.CDK_DEFAULT_ACCOUNT,
-  //   //   region: process.env.CDK_DEFAULT_REGION,
-  //   // },
-  //   /* Uncomment the next line if you know exactly what Account and Region you
-  //    * want to deploy the stack to. */
-  //   // env: { account: '123456789012', region: 'us-east-1' },
-  //   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+//new LodgeAppStack(app, "LodgeAppStack", {
+//   /* If you don't specify 'env', this stack will be environment-agnostic.
+//    * Account/Region-dependent features and context lookups will not work,
+//    * but a single synthesized template can be deployed anywhere. */
+//   /* Uncomment the next line to specialize this stack for the AWS Account
+//    * and Region that are implied by the current CLI configuration. */
+//   // env: {
+//   //   account: process.env.CDK_DEFAULT_ACCOUNT,
+//   //   region: process.env.CDK_DEFAULT_REGION,
+//   // },
+//   /* Uncomment the next line if you know exactly what Account and Region you
+//    * want to deploy the stack to. */
+//   // env: { account: '123456789012', region: 'us-east-1' },
+//   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+//});
 
-const { SamStack } = require('../lib/lodge-app-sam-stack');
-new SamStack(app, 'SamStack', {
+new KafkaStage(app, "KafkaStage", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
-  }
+  },
 });
